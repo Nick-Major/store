5ef9bc564d155c72779ea0599b53502a8448a760
Nick-Major <divanchik26@gmail.com>
2025-04-30 16:20:30 +0300

    second commit

---
+++ src/components/IconSwitch.jsx
@@ -0,0 +1,13 @@
+const IconSwitch = (icon, onSwitch) => {
+    return (
+        <button onClick={onSwitch} className="display-mode">
+            {icon === 'view_module' ? (
+                <i className="material-icons">view_module</i>
+                ) : (
+                <i className="material-icons">view_list</i>
+                )}
+        </button>
+    )
+}
+
+export default IconSwitch;
